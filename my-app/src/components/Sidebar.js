4e5ab8607033b57b1f1eceba6420cf9dd445f42a
rnc-personal <richard.challenger@protonmail.com>
2023-05-09 20:17:02 +0000

    sidebar styling

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,6 +1,7 @@
+import css from './css/Sidebar.module.css'
 
 const SideBar = (props) => {
-    return <div className="sidebar">
+    return <div className={css.sidebar}>
                 <a href={props.url}>1</a>
                 <a href={props.url}>2</a>
                 <a href={props.url}>3</a>
