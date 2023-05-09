5cd38b663c0a4ba97ad55835d12e702ee23e9ca4
rnc-personal <richard.challenger@protonmail.com>
2023-05-09 20:04:07 +0000

    Challenge 1

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+
+const SideBar = (props) => {
+    return <div className="sidebar">
+                <a href={props.url}>1</a>
+                <a href={props.url}>2</a>
+                <a href={props.url}>3</a>
+            </div>
+}
+
+
+export default SideBar
