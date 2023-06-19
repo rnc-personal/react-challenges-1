ef96c40edab97f96aed6fd3af6ae7ed6f90be7e4
rnc-personal <richard.challenger@protonmail.com>
2023-06-19 13:45:37 +0000

    hooks challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,14 @@ import Sidebar from './components/SideBar';
 // import NavBarSimple from './components/NavBarSimple';
 import NavBarSimplNavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarSimplNavBarForm />
       <Sidebar url="#"/>
-      <Content />
+      <ContentHooks />
     </div>
   );
 }
